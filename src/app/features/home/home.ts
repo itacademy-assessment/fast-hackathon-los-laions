fc18bff9889a0406992cedd1e6cc12e5204f3767
soyjuandelgado <soy.juan.delgado@gmail.com>
2025-07-31 10:56:08 +0000

    feat: repos in home

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
+import { GithubRepos } from '../../shared/services/github-repos';
+import { IRepository } from '../../shared/models/irepository';
 
 @Component({
   selector: 'app-home',
@@ -7,5 +9,41 @@ import { Component } from '@angular/core';
   styleUrl: './home.scss'
 })
 export class Home {
+  gitServ = inject(GithubRepos);
+  repos = computed<IRepository[]>(() => {
+    return [...this.reposJuan(), ...this.reposCarlosB(), ...this.reposCarlosM(), ...this.reposVania(), ...this.reposEdgar(), ...this.reposSergi()];
+  });
+  reposJuan = signal<IRepository[]>([])
+  reposCarlosM = signal<IRepository[]>([])
+  reposCarlosB = signal<IRepository[]>([])
+  reposVania = signal<IRepository[]>([])
+  reposEdgar = signal<IRepository[]>([])
+  reposSergi = signal<IRepository[]>([])
+  usuarios =["soyjuandelgado", "milenialdev", "Carlos-Martorell", "vaniaferreresteban"]
+  
+  constructor(){
+    this.obtenerRepos();
+  }
+
+  obtenerRepos(){
+    this.gitServ.getRepositories$("soyjuandelgado").subscribe((response) => {
+      this.reposJuan.set(response);
+    })
+    this.gitServ.getRepositories$("milenialdev").subscribe((response) => {
+      this.reposCarlosB.set(response);
+    })
+    this.gitServ.getRepositories$("Carlos-Martorell").subscribe((response) => {
+      this.reposCarlosM.set(response);
+    })
+    this.gitServ.getRepositories$("vaniaferreresteban").subscribe((response) => {
+      this.reposVania.set(response);
+    })
+    this.gitServ.getRepositories$("EdgarZerpaZG").subscribe((response) => {
+      this.reposEdgar.set(response);
+    })
+    this.gitServ.getRepositories$("sbondia").subscribe((response) => {
+      this.reposSergi.set(response);
+    })
+  }
 
 }
